7e0b96eaf07c8b787a60e4cb3c7b48daab6b8a7a
Pamella Bezerra <pamella@vinta.com.br>
2024-09-25 15:44:00 -0300

    Refactor Navbar to use more Mantine components

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,17 @@
-import { Code, Group, Text } from '@mantine/core';
+import { Code, Divider, Group, NavLink, Stack, Text } from '@mantine/core';
 import { useMedplum } from '@medplum/react';
-import { IconAmbulance, IconDashboard, IconHospital, IconLogout, IconStethoscope } from '@tabler/icons-react';
+import { Icon, IconAmbulance, IconDashboard, IconHospital, IconLogout, IconStethoscope } from '@tabler/icons-react';
 import { Link, useLocation } from 'react-router-dom';
 
 import classes from './Navbar.module.css';
 
-const menu = [
+interface MenuItem {
+  link: string;
+  label: string;
+  icon: Icon;
+}
+
+const menu: MenuItem[] = [
   { link: '/dashboard', label: 'Dashboard', icon: IconDashboard },
   { link: '/transfers', label: 'Transfer Center', icon: IconAmbulance },
   // { link: '/laboratory', label: 'Laboratory', icon: IconMicroscope },
@@ -13,12 +19,14 @@ const menu = [
   // { link: '/notifications', label: 'Notifications', icon: IconBellRinging },
 ];
 
-const adminMenu = [
+const adminMenu: MenuItem[] = [
   { link: '/physicians', label: 'Physicians', icon: IconStethoscope },
   { link: '/Location', label: 'Locations', icon: IconHospital },
   // { link: '/units', label: 'Units', icon: IconBuildingHospital },
   // { link: '/settings', label: 'Settings', icon: IconSettings },
 ];
+
+const logoutMenu: MenuItem = { link: '/signout', label: 'Logout', icon: IconLogout };
 
 export default function Navbar() {
   const { pathname } = useLocation();
@@ -27,49 +35,35 @@ export default function Navbar() {
   const activeItem =
     menu.find((item) => pathname.startsWith(item.link)) || adminMenu.find((item) => pathname.startsWith(item.link));
 
-  const links = menu.map((item) => (
-    <Link
-      className={classes.link}
-      data-active={item.link === activeItem?.link || undefined}
-      to={item.link}
-      key={item.label}
-    >
-      <item.icon className={classes.linkIcon} stroke={1.5} />
-      <span>{item.label}</span>
-    </Link>
-  ));
+  const links = menu.map((item) => renderLink(item));
 
-  const adminLinks = adminMenu.map((item) => (
-    <Link
-      className={classes.link}
-      data-active={item.link === activeItem?.link || undefined}
-      to={item.link}
-      key={item.label}
-    >
-      <item.icon className={classes.linkIcon} stroke={1.5} />
-      <span>{item.label}</span>
-    </Link>
-  ));
+  const adminLinks = adminMenu.map((item) => renderLink(item));
+
+  function renderLink(item: MenuItem): JSX.Element {
+    return (
+      <NavLink
+        className={classes.link}
+        component={Link}
+        active={item.link === activeItem?.link || undefined}
+        to={item.link}
+        key={item.label}
+        label={item.label}
+        leftSection={<item.icon className={classes.linkIcon} stroke={1.5} />}
+      />
+    );
+  }
 
   return (
-    <nav className={classes.navbar}>
-      <div className={classes.navbarMain}>
-        <Group className={classes.header} justify="space-between">
-          <Text>Transfer Center</Text>
-          <Code fw={700}>v1.0.1</Code>
-        </Group>
-        {links}
-      </div>
-      <div className={classes.horizontalLine}></div>
-
-      {isProjectAdmin ? <div className={classes.navbarMain}>{adminLinks}</div> : null}
-
-      <div className={classes.footer}>
-        <Link className={classes.link} to="/signout">
-          <IconLogout className={classes.linkIcon} stroke={1.5} />
-          <span>Logout</span>
-        </Link>
-      </div>
-    </nav>
+    <Stack>
+      <Group justify="space-between">
+        <Text>Transfer Center</Text>
+        <Code fw={700}>v1.0.1</Code>
+      </Group>
+      {links}
+      <Divider />
+      {isProjectAdmin ? adminLinks : null}
+      <Divider />
+      {renderLink(logoutMenu)}
+    </Stack>
   );
 }
