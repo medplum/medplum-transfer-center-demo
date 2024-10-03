23b9ac50a4a462a95908353e67776745c42972c8
Pamella Bezerra <pamella@vinta.com.br>
2024-10-03 11:50:54 -0300

    Upgrade packages and add new test-related ones

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,20 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
+    environment: 'jsdom',
     setupFiles: ['./src/test.setup.ts'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
